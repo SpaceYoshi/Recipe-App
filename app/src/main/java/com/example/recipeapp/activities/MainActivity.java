280e97471e022c99823964636715028759cbd92f
Max <maxhager@ziggo.nl>
2023-06-12 17:07:07 +0200

    Update

--- app/src/main/java/com/example/recipeapp/activities/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.recipeapp;
+package com.example.recipeapp.activities;
 
 import android.os.Bundle;
 
+import com.example.recipeapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
---
+++ app/src/main/java/com/example/recipeapp/activities/SearchActivity.java
@@ -0,0 +1,4 @@
+package com.example.recipeapp.activities;
+
+public class SearchActivity {
+}
